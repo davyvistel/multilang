75c729e17616fb46a098a61ba7516e9360a9a804
davyvistel <davy89250@gmail.com>
2024-06-17 15:47:39 +0200

    ajoue dans les setting des langues et creation d un templates

--- multilang_site/multilang_site/settings.py
+++ multilang_site/multilang_site/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
+]
+
+LOCALE_PATHS = [
+    BASE_DIR / 'locale',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
 ]
 
 ROOT_URLCONF = 'multilang_site.urls'
--- multilang_site/multilang_site/urls.py
+++ multilang_site/multilang_site/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from main import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('articles/', views.article_list, name='article_list'),
 ]
